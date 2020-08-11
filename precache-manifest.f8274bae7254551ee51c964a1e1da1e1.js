f22d4677f1198b769955bf75b4b2b2c331750650
Walter Tudorin <wtudorin@gmail.com>
2020-08-11 18:38:20 +1000

    Updates

---
+++ precache-manifest.f8274bae7254551ee51c964a1e1da1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0d9276db6566d32d05051983a7d6f5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b389659e7e56a83b891",
+    "url": "/monsters-rolodex/static/css/main.1d129be9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b389659e7e56a83b891",
+    "url": "/monsters-rolodex/static/js/main.e7457523.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
